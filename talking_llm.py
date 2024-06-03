ba2bd895d368f7ace508e366f00dd5b64e5f6928
DANI FILIPIN <43212605+devfilipin@users.noreply.github.com>
2024-06-03 13:13:47 -0400

    Update talking_llm.py
    adição da funcionalidade de gerar gráficos de barras através do eixo x e eixo y

--- talking_llm.py
+++ talking_llm.py
@@ -15,11 +15,9 @@ from langchain_experimental.agents.agent_toolkits import create_pandas_dataframe
 import pandas as pd  # Importa a biblioteca pandas para manipulação de dataframes
 from langchain.agents.agent_types import AgentType  # Importa a enumeração AgentType para especificar o tipo de agente
 
-# Carrega variáveis de ambiente de um arquivo .env
-load_dotenv(find_dotenv())
+load_dotenv(find_dotenv())  # Carrega variáveis de ambiente de um arquivo .env
 
-# Inicializa o cliente da OpenAI
-client = openai.Client()
+client = openai.Client()  # Inicializa o cliente da OpenAI
 
 class TalkingLLM():
     def __init__(self, model="gpt-3.5-turbo-0613", whisper_size="small"):
@@ -30,21 +28,16 @@ class TalkingLLM():
         self.dtype = 'int16'  # Define o tipo de dados do áudio
         self.display_callback = None  # Inicializa o callback para atualização da GUI como None
 
-        # Carrega o modelo Whisper para transcrição de áudio
-        self.whisper = whisper.load_model(whisper_size)
-        # Inicializa o modelo de linguagem ChatOpenAI
-        self.llm = ChatOpenAI(model=model)
-        # Inicializa a fila para armazenamento de respostas para TTS
-        self.llm_queue = Queue()
-        # Cria o agente para interagir com o dataframe
-        self.create_agent()
+        self.whisper = whisper.load_model(whisper_size)  # Carrega o modelo Whisper para transcrição de áudio
+        self.llm = ChatOpenAI(model=model)  # Inicializa o modelo de linguagem ChatOpenAI
+        self.llm_queue = Queue()  # Inicializa a fila para armazenamento de respostas para TTS
+        self.df = None  # Adiciona a propriedade para armazenar o dataframe
+        self.create_agent()  # Cria o agente para interagir com o dataframe
 
-        # Inicia a thread para conversão de texto em fala e reprodução do áudio
-        threading.Thread(target=self.convert_and_play, daemon=True).start()
+        threading.Thread(target=self.convert_and_play, daemon=True).start()  # Inicia a thread para conversão de texto em fala e reprodução do áudio
 
     def set_display_callback(self, callback):
-        # Define o callback para atualizar a GUI com as respostas
-        self.display_callback = callback
+        self.display_callback = callback  # Define o callback para atualizar a GUI com as respostas
 
     def start_or_stop_recording(self):
         # Alterna o estado de gravação e chama a função apropriada
@@ -64,8 +57,7 @@ class TalkingLLM():
         """
 
         try:
-            # Carrega o dataframe a partir de um arquivo CSV
-            df = pd.read_csv(df_path)
+            self.df = pd.read_csv(df_path)  # Carrega o dataframe
         except FileNotFoundError:
             print("Arquivo df_rent.csv não encontrado.")
             return
@@ -73,44 +65,38 @@ class TalkingLLM():
         # Cria um agente para interagir com o dataframe usando o modelo de linguagem
         self.agent = create_pandas_dataframe_agent(
             self.llm,
-            df,
+            self.df,
             prefix=agent_prompt_prefix,
             verbose=True,
             agent_type=AgentType.OPENAI_FUNCTIONS,
         )
 
     def load_new_dataframe(self, df_path):
-        # Carrega um novo dataframe e atualiza o agente
-        self.create_agent(df_path)
+        self.create_agent(df_path)  # Carrega um novo dataframe e atualiza o agente
 
     def save_and_transcribe(self):
         print("Saving the recording...")
         if "test.wav" in os.listdir():
             os.remove("test.wav")  # Remove qualquer arquivo de teste anterior
-        # Abre um novo arquivo WAV para escrita
-        wav_file = wave.open("test.wav", 'wb')
+        wav_file = wave.open("test.wav", 'wb')  # Abre um novo arquivo WAV para escrita
         wav_file.setnchannels(self.channels)  # Define o número de canais
         wav_file.setsampwidth(2)  # Define a largura de amostra (bytes por amostra)
         wav_file.setframerate(self.samplerate)  # Define a taxa de amostragem
-        # Escreve os dados de áudio no arquivo
-        wav_file.writeframes(np.array(self.audio_data, dtype=self.dtype))
+        wav_file.writeframes(np.array(self.audio_data, dtype=self.dtype))  # Escreve os dados de áudio no arquivo
         wav_file.close()  # Fecha o arquivo WAV
 
         try:
-            # Transcreve o áudio usando o modelo Whisper
-            result = self.whisper.transcribe("test.wav", fp16=False)
+            result = self.whisper.transcribe("test.wav", fp16=False)  # Transcreve o áudio usando o modelo Whisper
             user_text = result["text"]  # Obtém o texto transcrito
             print("Usuário:", user_text)
 
-            # Obtém a resposta do agente para o texto transcrito
-            response = self.agent.invoke(user_text)
+            response = self.agent.invoke(user_text)  # Obtém a resposta do agente para o texto transcrito
             jarvas_response = response['output']  # Obtém a resposta do agente
             print("AI:", jarvas_response)
             self.llm_queue.put(jarvas_response)  # Adiciona a resposta à fila de TTS
 
-            # Chama o callback para atualizar a GUI, se definido
             if self.display_callback:
-                self.display_callback(user_text, jarvas_response)
+                self.display_callback(user_text, jarvas_response)  # Chama o callback para atualizar a GUI, se definido
         except FileNotFoundError as e:
             print(f"Erro: {e}. Certifique-se de que o FFmpeg está instalado e no PATH do sistema.")
 
@@ -122,18 +108,17 @@ class TalkingLLM():
             if '.' in tts_text or '?' in tts_text or '!' in tts_text:
                 print(tts_text)
                 
-                # Converte o texto em fala usando a API da OpenAI
                 spoken_response = client.audio.speech.create(model="tts-1",
-                                                             voice='alloy', 
-                                                             response_format="opus",
-                                                             input=tts_text)
+                voice='alloy', 
+                response_format="opus",
+                input=tts_text
+                )
 
                 buffer = io.BytesIO()
                 for chunk in spoken_response.iter_bytes(chunk_size=4096):
                     buffer.write(chunk)  # Escreve os dados de áudio no buffer
                 buffer.seek(0)
 
-                # Reproduz o áudio usando a biblioteca soundfile e sounddevice
                 with sf.SoundFile(buffer, 'r') as sound_file:
                     data = sound_file.read(dtype='int16')
                     sd.play(data, sound_file.samplerate)
@@ -141,15 +126,13 @@ class TalkingLLM():
                 tts_text = ''  # Reseta o texto TTS
 
     def start_recording_thread(self):
-        # Inicia a thread de gravação
-        threading.Thread(target=self.run_recording, daemon=True).start()
+        threading.Thread(target=self.run_recording, daemon=True).start()  # Inicia a thread de gravação
 
     def run_recording(self):
         def callback(indata, frame_count, time_info, status):
             if self.is_recording:
                 self.audio_data.extend(indata.copy())  # Armazena os dados de áudio gravados
 
-        # Configura o stream de entrada de áudio
         with sd.InputStream(samplerate=self.samplerate, 
                             channels=self.channels, 
                             dtype=self.dtype, 
@@ -160,7 +143,6 @@ class TalkingLLM():
             def for_canonical(f):
                 return lambda k: f(l.canonical(k))
 
-            # Configura o hotkey para iniciar/parar a gravação
             hotkey = keyboard.HotKey(
                 keyboard.HotKey.parse('<cmd>'),
                 on_activate)
